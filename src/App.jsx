735bfcdee0c2b239e812259c3a92cb2f5cc9c6fd
Konstantin-Filipov <kosio1234545@gmail.com>
2023-08-16 18:35:37 +0300

    API configuration for GET

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import fetchData from './fetchData'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const { data, loading, error } = fetchData();
+  if(!data)
+  {
+    console.log(error);
+  }
   return (
     <>
       <div>
@@ -16,18 +21,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+      {data.map(movie => (
+          <li key={movie.id}>
+            {movie.title}
+          </li>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
