cebb69bd322de5fe058e7a206b73ec87ed10c2c0
Konstantin-Filipov <kosio1234545@gmail.com>
2023-08-30 18:03:35 +0200

    apply movie card style

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -5,15 +5,14 @@ function MovieCardInfo(props) {
   let movie = props.movie;
   let screening = props.screening;
   let keyIndex = props.itemKey;
-
+  //console.log(screenings)
   const imageUrl = `https://cinema-rest.nodehill.se/${movie.description.posterImage}`;
  
   return (
-    <div key={keyIndex}>
+    <div className="movie-card" key={keyIndex}>
       <div className='image'>
-        <Image  style={{height:"25rem", width:"18rem"}} src= {imageUrl} alt={movie.title} fluid/>
-        <div className='overlay'>
-        </div>
+        <Image style={{ height: "25rem", width: "18rem" }} src={imageUrl} alt={movie.title} fluid />
+        <div className='overlay'></div>
       </div>
       <div className='content'>
         <h3>{movie.title}</h3>
