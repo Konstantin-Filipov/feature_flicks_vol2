13f7e482485936bcd8044afdd46899cb2bbce029
Konstantin-Filipov <kosio1234545@gmail.com>
2023-08-31 14:46:48 +0200

    filter movies by category and render them

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,12 +3,26 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import MovieCardInfo from './movieCard';
 
+function filterMoviesByCategory(movies, selectedCategory) {
+    if (selectedCategory === null || selectedCategory === "All Categories") {
+      return movies; // Return all movies if no category is selected
+    }
+  
+    return movies.filter(movie => movie.description.categories.includes(selectedCategory));
+}
+  
+
 export default function AppMovies({movies, screenings, selectedCategory}){
-    //console.log(screenings.length)
-    console.log(selectedCategory);
+    //console.log(selectedCategory);
+
+    //filter the movies by category
+    let filteredMovies = filterMoviesByCategory(movies, selectedCategory);
+    console.log(filteredMovies);
+    
     // Create a map of screening dates to movies
     const screeningsByDate = {};
 
+    //create array for each date and push the appropriate screening to this date
     screenings.forEach((screening) => {
         const date = new Date(screening.time).toLocaleDateString();
         if (!screeningsByDate[date]) {
@@ -17,25 +31,30 @@ export default function AppMovies({movies, screenings, selectedCategory}){
         screeningsByDate[date].push(screening);
     });
 
-return (
-  <section id='movies' className='block movies-block'>
-    <Container fluid>{
-        Object.keys(screeningsByDate).map((date) => (
-            <div key={date}>
-                <h2>{date}</h2>
-                <Row>{
-                    screeningsByDate[date].map((screening) => {
-                    const movie = movies.find((m) => m.id === screening.movieId);
-                    return (
-                        <Col sm={3} key={screening.movieId}>
-                        <MovieCardInfo movie={movie} screening={screening} />
-                        </Col>
-                    );
-                })}
-                </Row>
-            </div>
-        ))}
-    </Container>
-  </section>
-);
+    return (
+    <section id='movies' className='block movies-block'>
+        <Container fluid>{
+            Object.keys(screeningsByDate).map((date) => (
+                <div key={date}>
+                    <h2>{date}</h2>
+                    <Row>{
+                        screeningsByDate[date].map((screening) => {
+                        const movie = filteredMovies.find((m) => m.id === screening.movieId);
+
+                        if (movie) {
+                            return (
+                                <Col sm={3} key={screening.movieId}>
+                                    <MovieCardInfo movie={movie} screening={screening} />
+                                </Col>
+                            );
+                        }
+                        
+                        return null; // Skip rendering if no valid movie is found
+                    })}
+                    </Row>
+                </div>
+            ))}
+        </Container>
+    </section>
+    );
 }
