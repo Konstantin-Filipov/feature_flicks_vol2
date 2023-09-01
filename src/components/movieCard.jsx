4ef7d2bb81da4f03e8f29c713e5494eb4539818a
Konstantin-Filipov <kosio1234545@gmail.com>
2023-09-01 20:38:13 +0200

    display each screening details

--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -1,13 +1,36 @@
 import Image from 'react-bootstrap/Image';
 
+function formatScreeningTime(screeningTime){
+
+  const myArray = screeningTime.split("T");
+  
+  let date = myArray[0];
+  let time = myArray[1].split(":00.");
+  time = time[0];
+
+  return `${date} ${time}`;
+}
+
+function formatMovieLength(lengthInMinutes) {
+  const hours = Math.floor(lengthInMinutes / 60);
+  const minutes = lengthInMinutes % 60;
+  
+  // Create the formatted string
+  const formattedMovieLength = ` ${hours}h ${minutes}min`;
+
+  return formattedMovieLength;
+}
 
 function MovieCardInfo(props) {
   let movie = props.movie;
   let screening = props.screening;
   let keyIndex = props.itemKey;
-  //console.log(screenings)
+  
   const imageUrl = `https://cinema-rest.nodehill.se/${movie.description.posterImage}`;
- 
+  
+  let screeningTime = formatScreeningTime(screening.time);
+  let movieLength = formatMovieLength(movie.description.length);
+
   return (
     <div className="movie-card" key={keyIndex}>
       <div className='image'>
@@ -16,7 +39,8 @@ function MovieCardInfo(props) {
       </div>
       <div className='content'>
         <h3>{movie.title}</h3>
-        <span className='screening_time'>{screening.time}</span>
+        <span className='screening_time'>{screeningTime}</span>
+        <span className='screening_length'>{movieLength}</span>
       </div>
     </div>
   );
